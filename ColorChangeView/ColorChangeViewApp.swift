98fc30c8803729e129d0613969fcf7e76e187d85
Briniel <lord.devol@gmail.com>
2021-10-29 12:17:02 +0300

    Initial Commit

---
+++ ColorChangeView/ColorChangeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorChangeViewApp.swift
+//  ColorChangeView
+//
+//  Created by Михаил Иванов on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorChangeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
